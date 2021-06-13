1ad4ba6ae7d340e6745532baada8c49cc1ea18df
heum-ji <dragon2009t@gmail.com>
2021-06-13 23:04:41 +0900

    test : 로그인 테스트 기능 추가
    @Test 추가하면 동시에 테스트 됨

--- spring_junit/src/test/java/kr/or/member/controller/MemberControllerTest.java
+++ spring_junit/src/test/java/kr/or/member/controller/MemberControllerTest.java
@@ -41,13 +41,28 @@ public class MemberControllerTest {
 
 		try {
 			mockMvc.perform(post("/join.do") // post로 join.do 컨트롤러 테스트
-					.param("memberId", "test01").param("memberPw", "1234").param("memberName", "테스트01")
+					.param("memberId", "test02").param("memberPw", "1234").param("memberName", "테스트01")
 					.param("phone", "01012341212").param("address", "테스트서울").param("gender", "여")) // 회원가입에 필요한 파라미터 전달
 					.andDo(print()) // 요청을 실행하고 결과를 출력
 					.andExpect(status().isOk()); // 예상 결과 값은 200(정상처리)
 			logger.info("회원가입 테스트 성공");
 		} catch (Exception e) {
 			logger.error("회원가입 테스트 실패");
+			// e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void loginTest() {
+		logger.info("로그인 테스트 시작");
+
+		try {
+			mockMvc.perform(post("/login.do").param("memberId", "test01").param("memberPw", "1234")).andDo(print())
+					.andExpect(status().isOk());
+
+			logger.info("로그인 테스트 성공");
+		} catch (Exception e) {
+			logger.error("로그인 테스트 실패!!!");
 			// e.printStackTrace();
 		}
 	}
